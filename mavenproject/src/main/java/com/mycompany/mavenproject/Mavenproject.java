30a011aa5d6e5332f90044a0a1c01ed5bbf6486e
ms23ait <ms23ait@COM-LB154-07.herts.ac.uk>
2026-02-23 14:34:56 +0000

    created a class

---
+++ mavenproject/src/main/java/com/mycompany/mavenproject/Mavenproject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject;
+
+/**
+ *
+ * @author ms23ait
+ */
+public class Mavenproject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
